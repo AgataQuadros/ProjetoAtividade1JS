83ce39e4b06df972aa7a00227ba708aeba1c4514
AgataQuadros <quadrosagata@gmail>
2025-05-08 21:35:15 -0300

    Calculadora - terminar

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
     
+    document.getElementById('somar').addEventListener('click', function() {})
+
+    document.getElementById('subtrair').addEventListener('click', function() {})
+
+
+    document.getElementById('multiplicar').addEventListener('click', function() {})
+
+
+    document.getElementById('dividir').addEventListener('click', function() {})
+
 });
